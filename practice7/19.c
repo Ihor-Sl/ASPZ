21a9f2b1f5b20d65ba82de3328d54f637e56c2b5
Ihor Sliunko <ihor.sliunko.dev@gmail.com>
2025-04-24 01:01:38 +0300

    pr7

--- practice7/19.c
+++ practice7/19.c
@@ -1,8 +1,45 @@
+#include <sys/types.h>
+#include <sys/sysctl.h>
+#include <sys/user.h>
+#include <libprocstat.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-int main() {
+int main(void) {
+    struct procstat *prstat;
+    struct kinfo_proc *p;
+    struct filestat_list *head;
+    struct filestat *fst;
+    unsigned int cnt;
 
-    printf("123");
+    prstat = procstat_open_sysctl();
+    if (prstat == NULL) {
+        perror("procstat_open_sysctl");
+        exit(EXIT_FAILURE);
+    }
 
+    p = procstat_getprocs(prstat, KERN_PROC_PROC, 0, &cnt);
+    if (p == NULL) {
+        perror("procstat_getprocs");
+        procstat_close(prstat);
+        exit(EXIT_FAILURE);
+    }
+
+    for (unsigned int i = 0; i < cnt; i++) {
+        head = procstat_getfiles(prstat, &p[i], 0);
+        if (head == NULL) continue;
+
+        STAILQ_FOREACH(fst, head, next) {
+            if (fst->fs_path != NULL)
+                printf("PID %d -> %s\n", p[i].ki_pid, fst->fs_path);
+        }
+
+        procstat_freefiles(prstat, head);
+    }
+
+    procstat_freeprocs(prstat, p);
+    procstat_close(prstat);
     return 0;
 }
